56109a71f952b087ca7bab068854e85e025c491c
Ahmed Nafie <101005449+AhmedNafie@users.noreply.github.com>
2024-11-11 13:57:12 +0200

    just commit

--- lib/core/api/app_configs/config_dev.dart
+++ lib/core/api/app_configs/config_dev.dart
@@ -1,4 +1,4 @@
 class DevConfig{
-  static const String baseUrl = 'https://moca-api-dev-ugqsn6ev6q-uc.a.run.app/api/v1/';
+  static const String baseUrl = 'https://moca-api-dev-29745357491.us-central1.run.app/api/v1/';
   static const String authBaseUrl = 'https://moca-testapi-dev.azurewebsites.net/api/';
 }
